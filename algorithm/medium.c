f6a88a58a279f83cbde5ea2c7b0bed99116b2aab
Eduardo Augusto <eduaugusto@proton.me>
2026-07-27 17:30:08 -0300

    wip: medium sort algorithm

--- algorithm/medium.c
+++ algorithm/medium.c
@@ -6,36 +6,50 @@
 /*   By: eduaaugu <eduaaugu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 16:47:27 by eduaaugu          #+#    #+#             */
-/*   Updated: 2026/07/23 18:52:55 by eduaaugu         ###   ########.fr       */
+/*   Updated: 2026/07/27 17:27:47 by eduaaugu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "algorithm.h"
 #include "operations.h"
 
-void	assign_index(t_stack *stack);
 int		ft_sqrt(int nb);
+void	assign_medium(t_stack *stack);
+void	a_rotation(int fd, t_stack *stack, t_stats *stats, int target);
+void	b_rotation(int fd, t_stack *stack, t_stats *stats, int target);
 
 void	sort_medium(t_stack *a, t_stack *b, t_stats *stats, int fd)
 {
-	int		rank;
-	t_node	*cursor;
+	int	chunk;
+	int	size;
+	int	target;
+	t_node	*current;
 
-	rank = ft_sqrt(a->size);
+	assign_medium(a);
+	chunk = 1;
+	size = ft_sqrt(a->size);
 	while (a->top)
 	{
-		cursor = a->top;
-		while (cursor->index < rank)
-			cursor = cursor->next;
-		while (a->top->value != cursor->value)
-			ra(fd, a, stats); // TODO Uma forma de usar rra quando for oportuno;
+		while (b->size < chunk * size) // BUG: Se raiz não for exata;
+		{
+			current = a->top;
+			while (current && chunk * size <= current->index)
+				current = current->next;
+			a_rotation(fd, a, stats, current->index); // BUG: se current == NULL;
+			pb(fd, b, a, stats);
+		}
+		chunk++;
+	}
+	target = b->size - 1;
+	while (b->top)
+	{
+		b_rotation(fd, b, stats, target);
 		pa(fd, a, b, stats);
-		if (b->size == rank) // TODO Esse 'if' não funciona em caso de raiz relativa;
-			rank += rank;
+		target--;
 	}
 }
 
-void	assign_index(t_stack *stack)
+void	assign_medium(t_stack *stack)
 {
 	t_node *cursor;
 	t_node *order;
@@ -56,7 +70,7 @@ void	assign_index(t_stack *stack)
 
 int	ft_sqrt(int nb)
 {
-	int	i;
+	long int	i;
 
 	if (nb <= 0)
 		return (0);
@@ -70,4 +84,44 @@ int	ft_sqrt(int nb)
 		i++;
 	}
 	return (0);
+}
+
+void	a_rotation(int fd, t_stack *stack, t_stats *stats, int target)
+{
+	int		target_position;
+	t_node	*cursor;
+
+	target_position = 0;
+	cursor = stack->top;
+	while (cursor->index != target)
+	{
+		target_position++;
+		cursor = cursor->next;
+	}
+	if (stack->size / 2 >= target_position)
+		while (stack->top->index != target)
+			ra(fd, stack, stats);
+	else
+		while (stack->top->index != target)
+			rra(fd, stack, stats);
+}
+
+void	b_rotation(int fd, t_stack *stack, t_stats *stats, int target) // Deletar e fazer uma função só;
+{
+	int		target_position;
+	t_node	*cursor;
+
+	target_position = 0;
+	cursor = stack->top;
+	while (cursor->index != target)
+	{
+		target_position++;
+		cursor = cursor->next;
+	}
+	if (stack->size / 2 >= target_position)
+		while (stack->top->index != target)
+			rb(fd, stack, stats);
+	else
+		while (stack->top->index != target)
+			rrb(fd, stack, stats);
 }
