57a9734f33a24c7ff508447c570e6ace6abd6d41
42edwa <eduaugusto@proton.me>
2026-07-28 19:02:36 -0300

    feat: implement algorithm utils; refactor: simple.c with utils

---
+++ algorithm/algorithm_utils.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eduaaugu <eduaaugu@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/28 12:49:23 by eduaaugu          #+#    #+#             */
+/*   Updated: 2026/07/28 18:53:59 by eduaaugu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "algorithm.h"
+
+void	assign_sorted_index(t_stack *stack)
+{
+	t_node	*cursor;
+	t_node	*order;
+
+	order = stack->top;
+	while (order)
+	{
+		cursor = stack->top;
+		while (cursor)
+		{
+			if (order->value >= cursor->value)
+				order->index++;
+			cursor = cursor->next;
+		}
+		order = order->next;
+	}
+}
+
+int	find_index_position(int index, t_stack *stack)
+{
+	int			position;
+	t_node		*current;
+
+	position = 0;
+	current = stack->top;
+	while (current && current->index != index)
+	{
+		position++;
+		current = current->next;
+	}
+	return (position);
+}
+
+void	rotate_to_top(int target, t_algorithm *alg,
+		int (*r)(int, t_stack *, t_stats *),
+		int (*rr)(int, t_stack *, t_stats *))
+{
+	int		position;
+	t_stack *stack;
+
+	if (!alg)
+		return ;
+	if (r == ra)
+		stack = alg->a;
+	else if (r == rb)
+		stack = alg->b;
+	else
+		return ;
+	if (!stack)
+		return ;
+	position = find_index_position(target, stack);
+	if (stack->size / 2 >= position)
+		while (stack->top->index != target)
+			r(alg->fd, stack, alg->stats);
+	else
+		while (stack->top->index != target)
+			rr(alg->fd, stack, alg->stats);
+}
--- algorithm/simple.c
+++ algorithm/simple.c
@@ -6,54 +6,25 @@
 /*   By: eduaaugu <eduaaugu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 17:44:22 by eduaaugu          #+#    #+#             */
-/*   Updated: 2026/07/23 16:37:37 by eduaaugu         ###   ########.fr       */
+/*   Updated: 2026/07/28 18:39:29 by eduaaugu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "algorithm.h"
 #include "operations.h"
 
-void	assign_index(t_stack *stack);
-
-void	sort_simple(t_stack *a, t_stack *b, t_stats *stats, int fd)
+void	sort_simple(t_algorithm *alg)
 {
-	t_node	*cursor;
-	t_node	*smallest;
+	int	i;
 
-	while (a->top)
+	i = 0;
+	assign_sorted_index(alg->a);
+	while (alg->a->top)
 	{
-		cursor = a->top;
-		smallest = a->top;
-		while (cursor)
-		{
-			cursor = cursor->next;
-			if (cursor && cursor->value < smallest->value)
-				smallest = cursor;
-		}
-		assign_index(a);
-		if (a->size / 2 >= smallest->index)
-			while(a->top->value != smallest->value)
-				ra(fd, a, stats);
-		else
-			while(a->top->value != smallest->value)
-				rra(fd, a, stats);
-		pb(fd, b, a, stats);
+		rotate_to_top(i, alg, ra, rra);
+		pb(alg->fd, alg->b, alg->a, alg->stats);
+		i++;
 	}
-	while(b->top)
-		pa(fd, a, b, stats);
+	while (alg->b->top)
+		pa(alg->fd, alg->a, alg->b, alg->stats);
 }
-
-void	assign_index(t_stack *stack)
-{
-	int		order;
-	t_node	*cursor;
-
-	order = 0;
-	cursor = stack->top;
-	while (cursor)
-	{
-		cursor->index = order++;
-		cursor = cursor->next;
-	}
-}
-
