116e1c263828f3ceaa5e4458df673c037547410f
42edwa <eduaugusto@proton.me>
2026-07-30 21:09:26 -0300

    feat: radix algorithm

--- algorithm/complex.c
+++ algorithm/complex.c
@@ -6,33 +6,63 @@
 /*   By: eduaaugu <eduaaugu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 10:45:45 by eduaaugu          #+#    #+#             */
-/*   Updated: 2026/07/30 16:49:10 by eduaaugu         ###   ########.fr       */
+/*   Updated: 2026/07/30 19:40:57 by eduaaugu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "algorithm.h"
 #include "operations.h"
 
-void	k_sort(t_algorithm *alg)
-{
-	int	delta;
+int	find_max_bits(int index);
 
+void	radix_sort(t_algorithm *alg)
+{
+	int	bit;
+	int	i;
+	int	max_bits;
+	int	size;
+
+	// Atribuir o índice de cada número na stack.
 	rank_index(alg->a);
-	delta = alg->a->size / 20 + 7;
-	while (alg->a->top)
+	
+	// Encontrar a quantidade de bits necessária para representar o maior índice.
+	bit = 0;
+	max_bits = find_max_bits(alg->a->size - 1);
+	size = alg->a->size;
+
+	// Percorrer toda a stack A uma vez.
+	while (bit < max_bits)
 	{
-		if (alg->a->top->index <= alg->b->size + delta)
+		i = 0;
+		// Repetir até analisar todos os elementos de A.
+		while (i < size)
 		{
-			pb(alg->b, alg->a, alg->stats);
-			if (alg->b->top->index <= alg->b->size)
-				rb(alg->b, alg->stats);
+			// Se o bit analisado for 0, empurrar o número para B.
+			if (((alg->a->top->index >> bit) & 1) == 0)
+				pb(alg->b, alg->a, alg->stats);
+
+			// Se o bit analisado for 1, rotacionar A.
+			else
+				ra(alg->a, alg->stats);
+			i++;
 		}
-		else
-			ra(alg->a, alg->stats);
+		// Passar todos os elementos de B de volta para A.
+		while(alg->b->top)
+			pa(alg->a, alg->b, alg->stats);
+		// Incrementar o bit analisado.
+		bit++;
 	}
-	while (alg->b->top)
+}
+
+int	find_max_bits(int index)
+{
+	int	max_bits;
+
+	max_bits = 0;
+	while (index > 0)
 	{
-		rotate_to_top(alg->b->size - 1, alg, rb, rrb);
-		pa(alg->a, alg->b, alg->stats);
+		max_bits++;
+		index >>= 1;
 	}
+	return (max_bits);
 }
